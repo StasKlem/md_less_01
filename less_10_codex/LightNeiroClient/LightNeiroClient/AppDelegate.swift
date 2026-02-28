1dd9dcf20ae1136c3978d9c3331392301dbe0296
StasKlem <stasmagic@ya.ru>
2026-02-28 15:50:32 +0500

    Replace storyboard with programmatic app startup

--- less_10_codex/LightNeiroClient/LightNeiroClient/AppDelegate.swift
+++ less_10_codex/LightNeiroClient/LightNeiroClient/AppDelegate.swift
@@ -7,24 +7,44 @@
 
 import Cocoa
 
-@main
+
 class AppDelegate: NSObject, NSApplicationDelegate {
+    private enum UI {
+        static let defaultWindowWidth: CGFloat = 900
+        static let defaultWindowHeight: CGFloat = 600
+    }
 
-    
-
+    private var window: NSWindow?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let viewController = ViewController()
+        let defaultFrame = NSRect(
+            x: 0,
+            y: 0,
+            width: UI.defaultWindowWidth,
+            height: UI.defaultWindowHeight
+        )
+        let window = NSWindow(
+            contentRect: defaultFrame,
+            styleMask: [.titled, .closable, .miniaturizable, .resizable],
+            backing: .buffered,
+            defer: false
+        )
+        window.isRestorable = false
+        window.setFrame(defaultFrame, display: true)
+        window.center()
+        window.setContentSize(NSSize(width: UI.defaultWindowWidth, height: UI.defaultWindowHeight))
+        window.title = "LightNeiroClient"
+        window.contentViewController = viewController
+        window.makeKeyAndOrderFront(nil)
+        NSApp.activate(ignoringOtherApps: true)
+        self.window = window
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
+        return false
     }
-
-
 }
-
--- less_10_codex/LightNeiroClient/LightNeiroClient/ViewController.swift
+++ less_10_codex/LightNeiroClient/LightNeiroClient/ViewController.swift
@@ -8,19 +8,45 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    private let titleLabel: NSTextField = {
+        let label = NSTextField(labelWithString: "LightNeiroClient")
+        label.font = NSFont.systemFont(ofSize: 28, weight: .semibold)
+        label.alignment = .center
+        return label
+    }()
+
+    private let subtitleLabel: NSTextField = {
+        let label = NSTextField(labelWithString: "UI is built programmatically without storyboard")
+        label.font = NSFont.systemFont(ofSize: 15, weight: .regular)
+        label.textColor = .secondaryLabelColor
+        label.alignment = .center
+        return label
+    }()
+
+    override func loadView() {
+        view = NSView()
+        view.wantsLayer = true
+        view.layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupLayout()
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    private func setupLayout() {
+        let stack = NSStackView(views: [titleLabel, subtitleLabel])
+        stack.orientation = .vertical
+        stack.alignment = .centerX
+        stack.spacing = 12
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stack)
+
+        NSLayoutConstraint.activate([
+            stack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stack.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 24),
+            stack.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -24)
+        ])
     }
-
-
 }
-
